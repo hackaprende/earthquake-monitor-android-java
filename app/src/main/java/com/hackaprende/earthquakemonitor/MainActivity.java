579443125b7cbb07685aa008ad368e2edeb38e80
Jesus Almaral <almaral17@gmail.com>
2020-12-10 12:03:21 -0600

    Add main screen design

--- app/src/main/java/com/hackaprende/earthquakemonitor/MainActivity.java
+++ app/src/main/java/com/hackaprende/earthquakemonitor/MainActivity.java
@@ -1,14 +1,17 @@
 package com.hackaprende.earthquakemonitor;
 
+import android.os.Bundle;
+
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.os.Bundle;
+import com.hackaprende.earthquakemonitor.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        ActivityMainBinding binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
     }
 }
