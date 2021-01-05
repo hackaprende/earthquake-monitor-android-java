6fac9345046d59c700d1b79a42dc48887e8d9c26
Jesus Almaral <almaral17@gmail.com>
2021-01-05 10:29:31 -0600

    Parse earthquakes JSON

--- app/src/main/java/com/hackaprende/earthquakemonitor/MainViewModel.java
+++ app/src/main/java/com/hackaprende/earthquakemonitor/MainViewModel.java
@@ -1,13 +1,16 @@
 package com.hackaprende.earthquakemonitor;
 
-import android.util.Log;
-
 import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
 import com.hackaprende.earthquakemonitor.api.EqApiClient;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
 import java.util.List;
 
 import retrofit2.Call;
@@ -28,7 +31,9 @@ public class MainViewModel extends ViewModel {
         service.getEarthquakes().enqueue(new Callback<String>() {
             @Override
             public void onResponse(Call<String> call, Response<String> response) {
-                Log.d("MainViewModel", response.body());
+                List<Earthquake> earthquakeList = parseEarthquakes(response.body());
+
+                eqList.setValue(earthquakeList);
             }
 
             @Override
@@ -36,7 +41,36 @@ public class MainViewModel extends ViewModel {
 
             }
         });
+    }
 
-        // this.eqList.setValue(eqList);
+    private List<Earthquake> parseEarthquakes(String responseString) {
+        ArrayList<Earthquake> eqList = new ArrayList<>();
+
+        try {
+            JSONObject jsonResponse = new JSONObject(responseString);
+            JSONArray featuresJSONArray = jsonResponse.getJSONArray("features");
+
+            for (int i = 0 ; i < featuresJSONArray.length() ; i++) {
+                JSONObject jsonFeature = featuresJSONArray.getJSONObject(i);
+                String id = jsonFeature.getString("id");
+                JSONObject jsonProperties = jsonFeature.getJSONObject("properties");
+
+                double magnitude = jsonProperties.getDouble("mag");
+                String place = jsonProperties.getString("place");
+                long time = jsonProperties.getLong("time");
+
+                JSONObject jsonGeometry = jsonFeature.getJSONObject("geometry");
+                JSONArray coordinatesJSONArray = jsonGeometry.getJSONArray("coordinates");
+                double longitude = coordinatesJSONArray.getDouble(0);
+                double latitude = coordinatesJSONArray.getDouble(1);
+
+                Earthquake earthquake = new Earthquake(id, place, magnitude, time, latitude, longitude);
+                eqList.add(earthquake);
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+
+        return eqList;
     }
 }
