5666d13a9963540550ca47884c77bb48c6a84eac
Jesus Almaral <almaral17@gmail.com>
2021-01-12 21:12:22 -0600

    Add MainRepository

---
+++ app/src/main/java/com/hackaprende/earthquakemonitor/MainRepository.java
@@ -0,0 +1,56 @@
+package com.hackaprende.earthquakemonitor;
+
+import com.hackaprende.earthquakemonitor.api.EarthquakeJSONResponse;
+import com.hackaprende.earthquakemonitor.api.EqApiClient;
+import com.hackaprende.earthquakemonitor.api.Feature;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
+
+class MainRepository {
+
+    public interface DownloadEqsListener {
+        void onEqsDownloaded(List<Earthquake> earthquakeList);
+    }
+
+    public void getEarthquakes(DownloadEqsListener downloadEqsListener) {
+        EqApiClient.EqService service = EqApiClient.getInstance().getService();
+
+        service.getEarthquakes().enqueue(new Callback<EarthquakeJSONResponse>() {
+            @Override
+            public void onResponse(Call<EarthquakeJSONResponse> call, Response<EarthquakeJSONResponse> response) {
+                List<Earthquake> earthquakeList = getEarthquakesWithMoshi(response.body());
+                downloadEqsListener.onEqsDownloaded(earthquakeList);
+            }
+
+            @Override
+            public void onFailure(Call<EarthquakeJSONResponse> call, Throwable t) {
+
+            }
+        });
+    }
+
+    private List<Earthquake> getEarthquakesWithMoshi(EarthquakeJSONResponse body) {
+        ArrayList<Earthquake> eqList = new ArrayList<>();
+
+        List<Feature> features = body.getFeatures();
+        for (Feature feature: features) {
+            String id = feature.getId();
+            double magnitude = feature.getProperties().getMag();
+            String place = feature.getProperties().getPlace();
+            long time = feature.getProperties().getTime();
+
+            double longitude = feature.getGeometry().getLongitude();
+            double latitude = feature.getGeometry().getLatitude();
+            Earthquake earthquake = new Earthquake(id, place, magnitude, time,
+                    latitude, longitude);
+            eqList.add(earthquake);
+        }
+
+        return eqList;
+    }
+}
--- app/src/main/java/com/hackaprende/earthquakemonitor/MainViewModel.java
+++ app/src/main/java/com/hackaprende/earthquakemonitor/MainViewModel.java
@@ -4,20 +4,7 @@ import androidx.lifecycle.LiveData;
 import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
-import com.hackaprende.earthquakemonitor.api.EarthquakeJSONResponse;
-import com.hackaprende.earthquakemonitor.api.EqApiClient;
-import com.hackaprende.earthquakemonitor.api.Feature;
-
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
 import java.util.List;
-
-import retrofit2.Call;
-import retrofit2.Callback;
-import retrofit2.Response;
 
 public class MainViewModel extends ViewModel {
 
@@ -27,73 +14,11 @@ public class MainViewModel extends ViewModel {
         return eqList;
     }
 
+    private final MainRepository repository = new MainRepository();
+
     public void getEarthquakes() {
-        EqApiClient.EqService service = EqApiClient.getInstance().getService();
-
-        service.getEarthquakes().enqueue(new Callback<EarthquakeJSONResponse>() {
-            @Override
-            public void onResponse(Call<EarthquakeJSONResponse> call, Response<EarthquakeJSONResponse> response) {
-                // List<Earthquake> earthquakeList = parseEarthquakes(response.body());
-
-                List<Earthquake> earthquakeList = getEarthquakesWithMoshi(response.body());
-                eqList.setValue(earthquakeList);
-            }
-
-            @Override
-            public void onFailure(Call<EarthquakeJSONResponse> call, Throwable t) {
-
-            }
+        repository.getEarthquakes(earthquakeList -> {
+            eqList.setValue(earthquakeList);
         });
-    }
-
-    private List<Earthquake> getEarthquakesWithMoshi(EarthquakeJSONResponse body) {
-        ArrayList<Earthquake> eqList = new ArrayList<>();
-
-        List<Feature> features = body.getFeatures();
-        for (Feature feature: features) {
-            String id = feature.getId();
-            double magnitude = feature.getProperties().getMag();
-            String place = feature.getProperties().getPlace();
-            long time = feature.getProperties().getTime();
-
-            double longitude = feature.getGeometry().getLongitude();
-            double latitude = feature.getGeometry().getLatitude();
-            Earthquake earthquake = new Earthquake(id, place, magnitude, time,
-                    latitude, longitude);
-            eqList.add(earthquake);
-        }
-
-        return eqList;
-    }
-
-    private List<Earthquake> parseEarthquakes(String responseString) {
-        ArrayList<Earthquake> eqList = new ArrayList<>();
-
-        try {
-            JSONObject jsonResponse = new JSONObject(responseString);
-            JSONArray featuresJSONArray = jsonResponse.getJSONArray("features");
-
-            for (int i = 0 ; i < featuresJSONArray.length() ; i++) {
-                JSONObject jsonFeature = featuresJSONArray.getJSONObject(i);
-                String id = jsonFeature.getString("id");
-                JSONObject jsonProperties = jsonFeature.getJSONObject("properties");
-
-                double magnitude = jsonProperties.getDouble("mag");
-                String place = jsonProperties.getString("place");
-                long time = jsonProperties.getLong("time");
-
-                JSONObject jsonGeometry = jsonFeature.getJSONObject("geometry");
-                JSONArray coordinatesJSONArray = jsonGeometry.getJSONArray("coordinates");
-                double longitude = coordinatesJSONArray.getDouble(0);
-                double latitude = coordinatesJSONArray.getDouble(1);
-
-                Earthquake earthquake = new Earthquake(id, place, magnitude, time, latitude, longitude);
-                eqList.add(earthquake);
-            }
-        } catch (JSONException e) {
-            e.printStackTrace();
-        }
-
-        return eqList;
     }
 }
